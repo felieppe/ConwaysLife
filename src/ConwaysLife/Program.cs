db5b6207402fe656cdf60ad02782fc1f6066a635
felieppe <me@felieppe.com>
2023-08-30 18:30:40 -0300

    Main program simplified

--- src/ConwaysLife/Program.cs
+++ src/ConwaysLife/Program.cs
@@ -1,15 +1,11 @@
-﻿using Library;
+﻿using System.Runtime.InteropServices;
+using Library;
 
 namespace ConwaysLife {
     class Program {
         static void Main(string[] args) {
-            string boardPath = "../../assets/board.txt";
-            Importer importer = new Importer(boardPath);
-
-            Board board = new Board(importer.LoadBoard());
-            Printer printer = new Printer(board, 20, 20);
-
-            printer.Print();
+            Screen screen = new Screen();
+            screen.ShowHomeMenu();
         }
     }
 }
