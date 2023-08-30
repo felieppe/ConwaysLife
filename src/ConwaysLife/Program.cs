6c9ae179b9bb180f1944e156d15f680a04c02f6c
felieppe <me@felieppe.com>
2023-08-30 11:49:35 -0300

    Added age parameter to PacientInfo class

--- src/ConwaysLife/Program.cs
+++ src/ConwaysLife/Program.cs
@@ -1,7 +1,24 @@
 ﻿namespace ConwaysLife {
     class Program {
         static void Main(string[] args) {
-            //
+            /*
+            
+                Needed classes:
+                    - Importer
+                        - LoadBoard()
+                    - Board
+                    - GameLogic
+                        - Game()
+                    - Printer
+                        - Print()
+
+                Cosas para hacer:
+                    - Agregar validacion de datos en los SETs.
+            
+            */
+
+            Importer imp = new Importer("");
+            Printer printer = new Printer(imp.LoadBoard(), 10, 10);
         }
     }
 }
